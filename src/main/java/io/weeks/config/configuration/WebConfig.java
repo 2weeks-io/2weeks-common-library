3250b9d37d1ebcc21475d645faecaae8ca921aa1
ByeonGooHoon <dksekfldks65@naver.com>
2020-02-12 21:01:59 +0900

    외부 리소스 접근 설정 등록

--- src/main/java/io/weeks/config/configuration/WebConfig.java
+++ src/main/java/io/weeks/config/configuration/WebConfig.java
@@ -1,16 +1,33 @@
 package io.weeks.config.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${weeks.fileUploadPath}")
+    private String resourcesLocation;
+
+    @Value("${weeks.fileUriPath}")
+    private String resourcesUri;
+
+    //CORS
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*");
+    }
+
+    //외부 리소스 매핑
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcesUri)
+                .addResourceLocations("file://" + resourcesLocation);
+
     }
 
 }

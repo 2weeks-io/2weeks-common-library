79ad2215169164d422d3fe346dd45420af7ea189
ByeonGooHoon <dksekfldks65@naver.com>
2020-02-13 01:06:55 +0900

    다른 서버 RESTFUL 기반 요청할 수 있도록 CONFIG 추가

---
+++ src/main/java/io/weeks/config/dto/RestfulConfigDto.java
@@ -0,0 +1,18 @@
+package io.weeks.config.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class RestfulConfigDto {
+
+    private int  readTimeout;
+
+    private int connectTimeout;
+
+    private int maxConnTotal;
+
+    private int maxConnPerRoute;
+
+}
